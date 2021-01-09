534775933ab765bb2487068c0daaec01a0323715
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2021-01-10 00:05:34 +0200

    Test: break build

---
+++ src/index.test.js
@@ -0,0 +1,26 @@
+import { expect } from "chai";
+import jsdom from "jsdom";
+import fs from "fs";
+
+describe("Our first test", () => {
+  it("should pass", () => {
+    expect(true).to.equal(true);
+  });
+});
+
+describe("index.html", () => {
+  it("should have h1 that says Users", (done) => {
+    const index = fs.readFileSync("./src/index.html", "utf-8");
+    // define jsdom enviroment
+    // this test is async return a callback function
+    // need to pass done
+    // to tell mocha to evaluate
+    jsdom.env(index, function (err, window) {
+      const h1 = window.document.getElementsByTagName("h1")[0];
+      expect(h1.innerHTML).to.equal("Hello :");
+      // tell mocha that our test is done
+      done();
+      window.close();
+    });
+  });
+});
